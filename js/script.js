2a0458f5389afba3d1af1a7c68ca3c97dd62daa4
SeymanurAkdogan <seymanurakdogan@outlook.com>
2023-08-21 10:58:34 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/* const backToTop = document.querySelector('.back-to-top');
+
+window.addEventListener('scroll', checkHeight)
+
+function checkHeight() {
+  if(wimdow.scrollY > 200) {
+    backToTop.style.display= "flex"
+  } else {
+    backToTop.style.display = "none"
+  }
+}
+
+backToTop.addEventListener('click', () => {
+  window.scrollTo({
+    top:0,
+    behavior: "smooth"
+  })
+})*/
